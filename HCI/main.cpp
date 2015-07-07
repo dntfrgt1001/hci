ae569515f735562e6b60a899b90127f17a94f0bb
masakazu nakazawa <masakazu@o196-249.pubnet.titech.ac.jp>
2015-07-07 12:32:32 +0900

    Initial Commit

---
+++ HCI/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  HCI
+//
+//  Created by masakazu nakazawa on 2015/07/07.
+//  Copyright (c) 2015年 masakazu. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
